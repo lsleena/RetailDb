c5f3adc1bbec92039105545e5562970a6d748b3d
leena <lsleena@gmail.com>
2017-01-10 13:01:03 -0800

    Initial Commit

---
+++ src/main/java/Add.java
@@ -0,0 +1,10 @@
+/**
+ * Created by leena on 1/10/17.
+ */
+public class Add {
+    public static void main(String args[]) {
+        int a =10,b =10;
+        System.out.print(a+b);
+
+    }
+}
---
+++ src/main/java/Hello.java
@@ -0,0 +1,10 @@
+/**
+ * Created by leena on 1/10/17.
+ */
+public class Hello {
+    public static void main(String args[]){
+        System.out.print("Hello leena");
+
+
+    }
+}
